ff7d3359fb96fca6b6755ec027fa239aaef5c6cd
lucasmbc <lucasmbc@hotmail.com>
2024-09-17 18:07:21 -0300

    First Commit

---
+++ area-retangulo.py
@@ -0,0 +1,7 @@
+print("\t========== Área retângulo ==========")
+base = int(input("\n\tDigite a área do retângulo: "))
+altura = int(input("\tDigite a altura do retângulo: "))
+
+areaRetangulo = base * altura
+
+print(f"\n\tA área de um retângulo com base de {base}cm e altura {altura}cm é {areaRetangulo}cm.")
